83b5abe49f884c5e9f03d93f1db09d0ff1053b3d
whoami <igorkzolov1992@gmail.com>
2024-12-24 16:46:40 +0500

    chapter 3.7

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
-use std::net::TcpListener;
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use actix_web::dev::Server;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::net::TcpListener;
+
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
     })
         .listen(listener)?
         .run();
@@ -15,4 +17,16 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
 
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
+}
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    #[allow(dead_code)]
+    email: String,
+    #[allow(dead_code)]
+    name: String,
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
